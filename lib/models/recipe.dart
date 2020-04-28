242e043f64e90f06ca91deff85d23aec3fe131a0
akinov <ayumoto10@gmail.com>
2020-04-29 00:38:34 +0900

    Add Recipe.all

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,7 +1,11 @@
+import 'package:dripper/sharedPref.dart';
+
 class Recipe {
   String name;
   int coffee;
   int water;
+
+  Recipe();
 
   String get ratio {
     String ratio = '0.0';
@@ -9,5 +13,22 @@ class Recipe {
       ratio = (water.toDouble() / coffee).toStringAsFixed(1);
     }
     return '1:$ratio';
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'coffee': coffee,
+        'water': water,
+      };
+
+  Recipe.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        coffee = json['coffee'],
+        water = json['water'];
+
+  static Future<List<Recipe>> all() async {
+    SharedPref sharedPref = SharedPref();
+    List list = await sharedPref.read('stringValue') ?? new List();
+    return list.map((json) => Recipe.fromJson(json)).toList();
   }
 }
