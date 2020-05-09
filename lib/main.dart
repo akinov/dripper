be1ed9f360360bc9767a79da0ce4419c62af705f
akinov <ayumoto10@gmail.com>
2020-05-09 22:32:00 +0900

    Add firebase analytics to Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home.dart';
@@ -5,6 +7,10 @@ import 'pages/home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,6 +21,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.brown,
           textTheme:
               GoogleFonts.notoSansTextTheme(Theme.of(context).textTheme)),
+      navigatorObservers: <NavigatorObserver>[observer],
     );
   }
 }
